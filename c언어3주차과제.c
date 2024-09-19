c0369be6a2a17d4a719ff767e3bd15c4f713fe30
wjsaaa0 <wjsaaa0@naver.com>
2024-09-19 17:35:42 +0900

    Add Text File [c언어3주차과제.c]

---
+++ c언어3주차과제.c
@@ -0,0 +1,17 @@
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	double km = 0;
+	double mile = 0;
+
+	printf("Please enter kilometers: ");
+	scanf("%lf", &km);
+
+	mile = 0.6215 * km;
+
+	printf("%.1lf km is equal to %.1lf miles", km, mile);
+	return 0;
+}
+*/
